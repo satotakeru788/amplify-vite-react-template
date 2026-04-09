8dd0e2fa8365c8f75a0e745b7f339c76bc00e315
sato <tk_satou@trust-coms.com>
2026-04-09 15:14:08 +0900

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
+  // const { signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -29,7 +29,9 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+
+    <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      {/* <h1>My todos</h1> */} 
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
